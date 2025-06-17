606f8f1bd2224f132c85a3b896a13373a498f63e
bishir tukur <bastukur1997@gmail.com>
2025-06-17 16:18:55 +0100

    update

--- src/pages/Home/Lisakq.jsx
+++ src/pages/Home/Lisakq.jsx
@@ -4,9 +4,15 @@ import image from "../../assets/w3.png";
 const Lisakq = () => {
   return (
     <div className='bg-white w-[100%] h-[100vh] flex items-center'>
-       <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 lg:px-36 md:px-10 px-6'>
+       <div className='grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 relative lg:px-36 md:px-10 px-6'>
+           {/* ball */}
+           <div className='w-8 h-8 bg-green rounded-full absolute top-10 left-32 animate-ping'></div>
+           <div className='w-8 h-8 bg-green rounded-full absolute top-10 right-32 animate-ping'></div>
+           <div className='w-8 h-8 bg-green rounded-full absolute top-32 right-64 animate-ping'></div>
+           <div className='w-8 h-8 bg-green rounded-full absolute top-52 right-80 animate-ping'></div>
+           {/* ball */}
           <img src={image} className='lg:w-[70%] md:w-[100%] w-fit' />
-          <div className='py-8 flex items-center'>
+          <div className='py-8 flex items-center relative z-10'>
             <div>
             <h1 className='lg:text-3xl md:text-2xl text-lg font-semibold mb-4'><span className='font-bold text-green'>LISAK</span> WATER COMPANY TREATS THE WATER WITH LATEST TECHNOLOGY.</h1>
             <p className='lg:text-sm md:text-sm text-xs font-semibold'>
